623ab84bd1422a84d09b76f226d63bb973e33dd7
Devante Lowery <dlowery2120@gmail.com>
2021-02-14 20:56:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,7 +3,10 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
+birthday_kids.each do |kids_name, age|
 
+puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
 def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
